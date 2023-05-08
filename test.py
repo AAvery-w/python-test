6698790c2a3b7c0747f6ff9a38184d1c4444a192
Avery <averyplonka@gmail.com>
2023-05-08 14:47:58 -0500

    List of Objectives added

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("this is a test")
+# list for this program:
+programDict = {
+    "print": "done",
+    "if-else": "DO",
+    "while loop": "DO",
+    "list": "DO",
+    "tuple": "DO",
+    "directory": "done",
+    "directory ADD": "do",
+    "set": "DO"
+}
